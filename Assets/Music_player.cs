f476df9419500bc16f26b61d3a23dcf0fe3e71d9
Joseph0606 <joseph7117@rocketmail.com>
2020-07-05 18:28:29 -0400

    Added ship, added splash screen, added music

---
+++ Assets/Music_player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Music_player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("LoadFirstScene", 4f);
+    }
+
+    void LoadFirstScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
