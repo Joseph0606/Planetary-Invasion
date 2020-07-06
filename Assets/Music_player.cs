c71c465c45450ce440c9b652882c4d9c832ea6b2
Joseph0606 <joseph7117@rocketmail.com>
2020-07-06 13:15:39 -0400

    Added camera movement intro

--- Assets/Music_player.cs
+++ Assets/Music_player.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Music_player : MonoBehaviour
 {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
