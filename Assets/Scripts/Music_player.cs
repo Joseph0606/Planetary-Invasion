c5f67da7d65651942d4a86256a639e5ae78d2b7c
Joseph0606 <joseph7117@rocketmail.com>
2020-07-12 19:22:41 -0400

    Organization and other changes

---
+++ Assets/Scripts/Music_player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Music_player : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+}
+
+    // Start is called before the first frame update
+
